f39d21c2e4adb7f61086a938f03d00fbd017f9b6
best.pid@gmail.com <best.pid@gmail.com>
2019-01-09 15:30:48 +0100

    second test  commit mohammed wahb a

--- srcipts/index.js
+++ srcipts/index.js
@@ -4,4 +4,44 @@ const stringToNum = x => {
     return parseInt(x);
 }
 
-module.exports = {stringToNum:stringToNum};
+// Create a class with the name of DCI_relatives that pass a role property. Two sublasses
+// that extends the DCI_relatives class. The trainer class and the student class.
+// Trainers class should have role='Trainer', student's should have their role='Student',
+// Both subclasses have private properties, such as firstName and lastName.
+// Create two instances, one trainer, and one student. and pass them as arguments to
+// the previous function that returns an object. Do not stop until both tests pass.
+
+class DCI_relatives {
+    constructor (role){
+        this.role=role;
+    }
+}
+
+class trainer extends DCI_relatives {
+    constructor (firstName, lastName){
+        super("trainer");
+        this.firstName=firstName;
+        this.lastName=lastName;
+        
+    }
+}
+
+class student extends DCI_relatives {
+    constructor (firstName, lastName){
+        super("student");
+        this.firstName=firstName;
+        this.lastName=lastName;
+        
+    }
+}
+
+let kostas= new trainer("kostas","diakogiannis");
+let mohammed= new student("mohammed","wahba");
+
+
+
+module.exports = {
+    stringToNum:stringToNum,
+     kostas:kostas,
+      mohammed:mohammed
+    };
--- test/specs.js
+++ test/specs.js
@@ -1,7 +1,7 @@
-const assert = require('assert');
+const assert = require('assert'); // node.js
 const index = require('../srcipts/index.js');
 
-describe('Whole test functions', ()=>{
+describe('Whole test functions', ()=>{   // mocha
 
     describe('just a test', ()=>{
 
@@ -9,6 +9,12 @@ describe('Whole test functions', ()=>{
             const actualResult=index.stringToNum("7");
             const expectedResult=7;
             assert.strictEqual(actualResult,expectedResult);
+        })
+
+        it('to test deepEqual for Array and Object', ()=>{
+            const actualResult=index.mohammed;
+            const expectedResult={firstName:"mohammed", lastName:"wahba", role:"student"};
+            assert.deepEqual(actualResult,expectedResult);
         })
 
     })
